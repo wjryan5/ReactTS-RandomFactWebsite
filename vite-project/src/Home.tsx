fc6b3fbc7293460f6d23c0e47328ca222b0abc37
wjryan5 <43756110+wjryan5@users.noreply.github.com>
2022-11-25 15:42:13 +0000

    API connected and working
    Using Axion

--- vite-project/src/Home.tsx
+++ vite-project/src/Home.tsx
@@ -1,24 +1,32 @@
-const Home = () => {
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '458d304e3bmsh5348829f8d379b1p138a54jsn0f0c995f2b45',
-            'X-RapidAPI-Host': 'random-facts2.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://random-facts2.p.rapidapi.com/getfact', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+const url = `https://random-facts2.p.rapidapi.com/getfact`;
 
-    return ( 
-        <div className="home">
-            <h3>Get your random facts here!</h3>
-            <p></p>
-        </div>
-     );
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "458d304e3bmsh5348829f8d379b1p138a54jsn0f0c995f2b45",
+    "X-RapidAPI-Host": "random-facts2.p.rapidapi.com",
+  },
+};
+
+function Home() {
+  const [facts, setFact] = useState();
+
+  const handleClick = async () => {
+    const data = await axios.get(url, options);
+    setFact(data);
+  };
+
+  console.log(facts);
+  return (
+    <div className="container">
+      <h2>Get your Random Facts Here!</h2>
+      <button onClick={handleClick}>Get Fact</button>
+      <div>{JSON.stringify(facts && facts.data.Fact)}</div>
+    </div>
+  );
 }
- 
+
 export default Home;
